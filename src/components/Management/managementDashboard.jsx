f70468002e9b93a9a2d9f67aa70f96d88c823619
Darkdevilsp <b520066@iiit-bh.ac.in>
2024-02-07 19:02:41 +0530

    management
    Signed-off-by: Darkdevilsp <b520066@iiit-bh.ac.in>

--- src/components/Management/managementDashboard.jsx
+++ src/components/Management/managementDashboard.jsx
@@ -1,4 +1,20 @@
-import {useParams} from "react-router-dom";
+import {Route, Routes, useParams} from "react-router-dom";
+import Staffs from "../../Pages/Staffs/Staffs";
+import Appointment from "../../Pages/Appointments/Appointment";
+import Dashboard from "../../Pages/Dashboard/Dashboard";
+import AddDoctor from "../../Pages/Doctors/AddDoctor/AddDoctor";
+import Registration from "../../Pages/Login/Registration/Registration";
+import PatientViewDoctor from "../../Pages";
+import Login from "../../Pages/Login/Login";
+import PageNotFound from ".../../Pages/PageNotFound/PageNotFound";
+import ViewDoctors from "../../Pages/Patients/ViewDoctors/ViewDoctors";
+import DeleteDoctor from "../../Pages/Doctors/DeleteDoctor/DeleteDoctor";
+import FindDoctor from "../../Pages/Doctors/UpdateDoctor/FindDoctor";
+import AddPatient from "../../Pages/Patients/AddPatients/AddPatient";
+import Patients from "../../Pages/Patients/Patients/Patients";
+import PatientDetails from "../../Pages/Patients/PatientsDetails/PatientDetails";
+import ApproveDoctor from "../../Pages/Doctors/ApproveDoctor/ApproveDoctor";
+import './App.css';
 
 
 function ManagementDashboard() {
@@ -6,6 +22,28 @@ function ManagementDashboard() {
     return(
         <div>
             <h1>welcome {username}</h1>
+            <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="doctors" element={<PatientViewDoctor />} />
+            <Route path="addDoctor" element={<AddDoctor />} />
+            <Route path="approveDoctor" element={<ApproveDoctor />} />
+            <Route path="deleteDoctor" element={<DeleteDoctor />} />
+            <Route path="updateDoctor" element={<FindDoctor />} />
+            <Route path="patients" element={<Patients />} />
+            <Route path="addPatient" element={<AddPatient />}>
+              <Route path=":email" element={<AddPatient />} />
+            </Route>
+            <Route path="viewDoctors" element={<ViewDoctors />} />
+            <Route path="patientDetails/:id" element={<PatientDetails />} />
+            <Route path="staffs" element={<Staffs />} />
+            <Route path="appointment" element={<Appointment />}>
+              <Route path=":email" element={<Appointment />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="registration" element={<Registration />} />
+            <Route path="*" element={<PageNotFound />} />
+
+            </Routes>
         </div>
     )
 }
