b9087e1764ecbcb366de3030b8ea08990f28fc9a
MIRIYALA CHARAN SAI <mchs10987.2001@gmail.com>
2024-02-13 09:55:47 +0530

    doctor manaagement

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home.jsx';
 import PatientLogin from './components/Patients/patientLogin.jsx';
@@ -13,8 +13,7 @@ import PatientDashboard from "./components/Patients/patientDashboard.jsx";
 import DoctorDashboard from "./components/Doctors/doctorDashboard.jsx";
 import ManagementDashboard from "./components/Management/managementDashboard.jsx";
 import AppointmentBooking from "./components/Patients/appointmentBooking.jsx";
-
-
+import Doctors from "./components/Management/doctors.jsx";
 const App = () => {
 
     const [user, setUser] = useState(sessionStorage.getItem('user') || "None");
@@ -31,38 +30,46 @@ const App = () => {
 
     return (
         <div className={"completebody"}>
-            <Router>
-                <div>
-                    <div className="topnav">
-                        <Link to="/"><button>HOME</button></Link>
-                        {user === "None" ?
+        <Router>
+            <div>
+                <div className="topnav">
+                    <Link to="/"><button>HOME</button></Link>
+                    {user==="None"?
+                        <div>
+                            <Link to="/patientLogin"><button>PATIENT LOGIN</button></Link>
+                            <Link to="/doctorLogin"><button>DOCTOR LOGIN</button></Link>
+                            <Link to="/managementLogin"><button>MANAGEMENT LOGIN</button></Link>
+                        </div>
+                        :user==="management"?
                             <div>
-                                <Link to="/patientLogin"><button>PATIENT LOGIN</button></Link>
-                                <Link to="/doctorLogin"><button>DOCTOR LOGIN</button></Link>
-                                <Link to="/managementLogin"><button>MANAGEMENT LOGIN</button></Link>
+                                <Link to={"/m/:username/patients"} ><button>PATIENTS</button></Link>
+                                <Link to={"/m/:username/doctors"}><button>DOCTORS</button></Link>
+                                <Link to={"/m/:username/staff"}><button>STAFF</button></Link>
+                                <Link to={"/"}> <button onClick={handleLogout}>LOGOUT</button> </Link>
+
                             </div>
                             :
-                            <Link to={"/"}> <button onClick={handleLogout}>LOGOUT</button> </Link>
-                        }
-
-                    </div>
-
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/patientLogin" element={<PatientLogin user={user} setuser={setUser} />} />
-                        <Route path="/doctorLogin" element={<DoctorLogin user={user} setuser={setUser} />} />
-                        <Route path="/managementLogin" element={<ManagementLogin user={user} setuser={setUser} />} />
-                        <Route path="/patientSignup" element={<PatientSignup user={user} setuser={setUser} />} />
-                        <Route path="/managementSignup" element={<ManagementSignup user={user} setuser={setUser} />} />
-                        <Route path="/doctorSignup" element={<DoctorSignup user={user} setuser={setUser} />} />
-                        <Route path="/p/:username/Dashboard" element={<PatientDashboard user={user} setuser={setUser} />} />
-                        <Route path="/d/:username/Dashboard" element={<DoctorDashboard user={user} setuser={setUser} />} />
-                        <Route path="/m/:username/Dashboard/*" element={<ManagementDashboard user={user} setuser={setUser} />} />
-                        <Route path="/p/:username/appointment" element={<AppointmentBooking />} />
-                    </Routes>
+                                 <Link to={"/"}> <button onClick={handleLogout}>LOGOUT</button> </Link>
+                    }
 
                 </div>
-            </Router>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path={"/patientLogin"} element={<PatientLogin user={user} setuser={setUser}/>} />
+                    <Route path={"/doctorLogin"} element={<DoctorLogin user={user} setuser={setUser}/>} />
+                    <Route path={"/managementLogin"} element={<ManagementLogin user={user} setuser={setUser}/>} />
+                    <Route path={"/patientSignup"} element={<PatientSignup user={user} setuser={setUser}/>}/>
+                    <Route path={"/managementSignup"} element={<ManagementSignup user={user} setuser={setUser}/>}/>
+                    <Route path={"/doctorSignup"} element={<DoctorSignup user={user} setuser={setUser}/>}/>
+                    <Route path={"/p/:username/Dashboard"} element={<PatientDashboard user={user} setuser={setUser}/>}/>
+                    <Route path={"/d/:username/Dashboard"} element={<DoctorDashboard user={user} setuser={setUser}/>}/>
+                    <Route path={"/m/:username/Dashboard"} element={<ManagementDashboard user={user} setuser={setUser}/>}/>
+                    <Route path={"/p/:username/appointment"} element={<AppointmentBooking />} />
+                    <Route path={"/m/:username/doctors"} element={<Doctors/>}/>
+                </Routes>
+            </div>
+        </Router>
         </div>
     );
 };
