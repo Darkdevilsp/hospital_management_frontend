b5ae0304892a5e2a563872afd1de5fa7d6590e21
srinivasroutu <srinivasroutu100@gmail.com>
2024-05-11 22:05:40 +0530

    hdp

--- src/components/Patients/hdp.jsx
+++ src/components/Patients/hdp.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import './hdp.css';
+import heart from "../../utils/heart.png"
+import { Box, Container, TextField, Button, Typography, Select, MenuItem } from '@mui/material'; // Import Material-UI components
+
 
 const Hdp = () => {
     const [formData, setFormData] = useState({
@@ -42,10 +44,10 @@ const Hdp = () => {
             }
             const data = await response.json();
             setPrediction(data.prediction);
-            if(prediction===0){
+            if (prediction === 1) {
                 alert("You may have a disease consult a doctor")
             }
-            else{
+            else {
                 alert("You are healthy")
             }
         } catch (error) {
@@ -56,89 +58,126 @@ const Hdp = () => {
 
 
     return (
-        <div className="form-container">
-            <h1>Heart Disease Form</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="age">Age</label>
-                <input type="text" id="age" name="age" value={formData.age} onChange={handleChange}
-                       placeholder="Your age.."/><br/>
+        <div style={{
+            backgroundImage: `url(${heart})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Box className="form-container" style={{
+                width: '400px',
+                padding: '20px',
+                borderRadius: '10px',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                color: 'black',
+                position: 'relative',
+                left: '25%',
+                textAlign: 'left'
+            }}>
+                <h1 style={{
+                    color: "purple"
+                }}>Heart Disease Form</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="age">Age</label>
+                    <input type="text" id="age" name="age" value={formData.age} onChange={handleChange}
+                        placeholder="Your age.." /><br />
+                    <div style={{
+                        float: 'left',
 
-                <label htmlFor="sex">Sex</label>
-                <select id="sex" name="sex" value={formData.sex} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="1">Male</option>
-                    <option value="0">Female</option>
-                </select><br/>
+                    }}>
+                        <label htmlFor="sex">Sex</label>
+                        <select id="sex" name="sex" value={formData.sex} onChange={handleChange} style={{width: '100%', padding: '10px'}}>
+                            <option value="">----select option----</option>
+                            <option value="1">Male</option>
+                            <option value="0">Female</option>
+                        </select>
+                    </div>
+                    <div style={{
+                        float: 'right',
+                        position: 'absolute',
+                        width: '100%',
+                        
+                        
+                    }}>
+                        <label htmlFor="cp" style={{textAlign: 'center'}} >Chest Pain Type</label>
+                        <select id="cp" name="cp" value={formData.cp} onChange={handleChange} style={{width: '50%', padding: '10px', marginLeft: '30%'}}>
+                            <option value="">----select option----</option>
+                            <option value="0">Typical Angina</option>
+                            <option value="1">Atypical Angina</option>
+                            <option value="2">Non-anginal Pain</option>
+                            <option value="3">Asymptomatic</option>
+                        </select>
+                    </div>
+                    <label htmlFor="trestbps">Resting Blood Pressure</label>
+                    <input type="text" id="trestbps" name="trestbps" value={formData.trestbps} onChange={handleChange}
+                        placeholder="A number in range [94-200] mmHg" /><br />
 
-                <label htmlFor="cp">Chest Pain Type</label>
-                <select id="cp" name="cp" value={formData.cp} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="0">Typical Angina</option>
-                    <option value="1">Atypical Angina</option>
-                    <option value="2">Non-anginal Pain</option>
-                    <option value="3">Asymptomatic</option>
-                </select><br/>
+                    <label htmlFor="chol">Serum Cholesterol</label>
+                    <input type="text" id="chol" name="chol" value={formData.chol} onChange={handleChange}
+                        placeholder="A number in range [126-564] mg/dl" /><br />
 
-                <label htmlFor="trestbps">Resting Blood Pressure</label>
-                <input type="text" id="trestbps" name="trestbps" value={formData.trestbps} onChange={handleChange}
-                       placeholder="A number in range [94-200] mmHg"/><br/>
+                    <label htmlFor="fbs">Fasting Blood Sugar</label>
+                    <select id="fbs" name="fbs" value={formData.fbs} onChange={handleChange} style={{width: '100%', padding: '10px'}}>
+                        <option value="">----select option----</option>
+                        <option value="1">Greater than 120 mg/dl</option>
+                        <option value="0">Less than 120 mg/dl</option>
+                    </select><br />
 
-                <label htmlFor="chol">Serum Cholesterol</label>
-                <input type="text" id="chol" name="chol" value={formData.chol} onChange={handleChange}
-                       placeholder="A number in range [126-564] mg/dl"/><br/>
+                    <label htmlFor="restecg">Resting ECG Results</label>
+                    <select id="restecg" name="restecg" value={formData.restecg} onChange={handleChange} style={{width: '100%', padding: '10px'}}>
+                        <option value="">----select option----</option>
+                        <option value="0">Normal</option>
+                        <option value="1">Having ST-T wave abnormality</option>
+                        <option value="2">Probable or definite left ventricular hypertrophy</option>
+                    </select><br />
 
-                <label htmlFor="fbs">Fasting Blood Sugar</label>
-                <select id="fbs" name="fbs" value={formData.fbs} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="1">Greater than 120 mg/dl</option>
-                    <option value="0">Less than 120 mg/dl</option>
-                </select><br/>
+                    <label htmlFor="thalach">Max Heart Rate</label>
+                    <input type="text" id="thalach" name="thalach" value={formData.thalach} onChange={handleChange}
+                        placeholder="A number in range [71-202] bpm" /><br />
 
-                <label htmlFor="restecg">Resting ECG Results</label>
-                <select id="restecg" name="restecg" value={formData.restecg} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="0">Normal</option>
-                    <option value="1">Having ST-T wave abnormality</option>
-                    <option value="2">Probable or definite left ventricular hypertrophy</option>
-                </select><br/>
+                    <label htmlFor="exang">Exercise-induced Angina</label>
+                    <select id="exang" name="exang" value={formData.exang} onChange={handleChange} style={{width: '100%', padding: '10px'}}>
+                        <option value="">----select option----</option>
+                        <option value="1">Yes</option>
+                        <option value="0">No</option>
+                    </select><br />
 
-                <label htmlFor="thalach">Max Heart Rate</label>
-                <input type="text" id="thalach" name="thalach" value={formData.thalach} onChange={handleChange}
-                       placeholder="A number in range [71-202] bpm"/><br/>
+                    <label htmlFor="oldpeak">ST Depression</label>
+                    <input type="text" id="oldpeak" name="oldpeak" value={formData.oldpeak} onChange={handleChange}
+                        placeholder="ST depression, typically in [0-6.2]" /><br />
 
-                <label htmlFor="exang">Exercise-induced Angina</label>
-                <select id="exang" name="exang" value={formData.exang} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="1">Yes</option>
-                    <option value="0">No</option>
-                </select><br/>
+                    <label htmlFor="slope">Slope of the peak exercise ST segment</label>
+                    <select id="slope" name="slope" value={formData.slope} onChange={handleChange} style={{width: '100%', padding: '10px'}}>
+                        <option value="">----select option----</option>
+                        <option value="0">Upsloping</option>
+                        <option value="1">Flat</option>
+                        <option value="2">Downsloping</option>
+                    </select><br />
 
-                <label htmlFor="oldpeak">ST Depression</label>
-                <input type="text" id="oldpeak" name="oldpeak" value={formData.oldpeak} onChange={handleChange}
-                       placeholder="ST depression, typically in [0-6.2]"/><br/>
+                    <label htmlFor="ca">Number of Major Vessels</label>
+                    <input type="text" id="ca" name="ca" value={formData.ca} onChange={handleChange}
+                        placeholder="Typically in [0-4]" /><br />
 
-                <label htmlFor="slope">Slope of the peak exercise ST segment</label>
-                <select id="slope" name="slope" value={formData.slope} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="0">Upsloping</option>
-                    <option value="1">Flat</option>
-                    <option value="2">Downsloping</option>
-                </select><br/>
-
-                <label htmlFor="ca">Number of Major Vessels</label>
-                <input type="text" id="ca" name="ca" value={formData.ca} onChange={handleChange}
-                       placeholder="Typically in [0-4]"/><br/>
-
-                <label htmlFor="thal">Thalassemia</label>
-                <select id="thal" name="thal" value={formData.thal} onChange={handleChange}>
-                    <option value="">----select option----</option>
-                    <option value="0">Normal</option>
-                    <option value="1">Fixed Defect</option>
-                    <option value="2">Reversible Defect</option>
-                </select><br/>
-
-                <input type="submit" className="my-cta-button" value="Predict"/>
-            </form>
+                    <label htmlFor="thal">Thalassemia</label>
+                    <select id="thal" name="thal" value={formData.thal} onChange={handleChange} style={{width: '100%', padding: '10px', marginBottom: '10px'}}>
+                        <option value="">----select option----</option>
+                        <option value="0">Normal</option>
+                        <option value="1">Fixed Defect</option>
+                        <option value="2">Reversible Defect</option>
+                    </select><br />
+                    <Button
+                        type="submit"
+                        value="Predict"
+                        variant="contained"
+                        color="secondary"
+                        fullWidth
+                    >
+                        Login
+                    </Button>
+                </form>
+            </Box>
         </div>
     );
 };
